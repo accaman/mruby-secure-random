d7113c9640606f89041f472a2590428e94b4dc6c
accaman <ihorolog@gmail.com>
2021-02-24 19:57:13 +0900

    fixed random_bytes method to avoid system stack error

--- mrblib/mrb_securerandom.rb
+++ mrblib/mrb_securerandom.rb
@@ -1,21 +1,14 @@
 class SecureRandom
   class << self
     def random_bytes(n=nil)
-      n = n ? n.to_int : 16
-
-      begin
-        File.open("/dev/urandom", 'r') {|f|
-          ret = f.read(n)
-          unless ret.length == n
-            raise NotImplementedError, "Unexpected partial read from random device: only #{ret.length} for #{n} bytes"
-          end
-          return ret
-        }
-      rescue
-        raise File::NoFileError, "No random device"
+      n = n ? n.to_i : 16
+      b = File.open("/dev/urandom", "r") { |fh| fh.read(n) }
+      if b.size != n
+        raise "Unexpected partial read from random device: only #{ b.size } for #{ n } bytes"
       end
-
-      raise NotImplementedError, "No random device"
+      b
+    rescue
+      raise File::NoFileError, "No random device"
     end
 
     def hex(n=nil)
